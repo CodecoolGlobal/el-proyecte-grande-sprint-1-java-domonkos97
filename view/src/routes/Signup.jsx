67f5d34bc5cd8299690269049b3fc1bc2143f7c5
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-06-12 14:33:49 +0200

    Refactor login & register component

--- view/src/routes/Signup.jsx
+++ view/src/routes/Signup.jsx
@@ -4,7 +4,7 @@ import GoogleIcon from "@mui/icons-material/Google.js";
 import FacebookIcon from "@mui/icons-material/Facebook.js";
 import GitHubIcon from "@mui/icons-material/GitHub.js";
 
-const Register = () => {
+const Signup = () => {
 
     const [username, setUsername] = useState("");
     const [firstName, setFirstName] = useState("");
@@ -32,11 +32,10 @@ const Register = () => {
     // send post request with username, firstname, lastname
 
     return  (
-        <div>
-            <div className="toolbar-height"></div>
+        <div className="form-container">
             <div className='container-login'>
                 <form onSubmit={createNewUser}>
-                    <h4 style={{color: 'white', margin: '100px auto 50px auto', textAlign:'center'}}>Sign Up</h4>
+                    <h4 style={{color: 'white', margin: '50px auto 50px', textAlign:'center'}}>Sign Up</h4>
                     <div className="socials">
                         <Link to='google.com' className='social-button'><GoogleIcon></GoogleIcon></Link>
                         <Link to='google.com' className='social-button'><FacebookIcon></FacebookIcon></Link>
@@ -52,10 +51,10 @@ const Register = () => {
                         <input type="text" placeholder='Last Name'  className='input-field' required onChange={(event) => {setLastName(event.target.value)}} value={lastName}/>
                         <input type="password" placeholder='****'  className='input-field' required onChange={(event) => {setPassword((event.target.value))}
                         } value={password}/>
+                        <input type="password" placeholder='****'  className='input-field' required />
                         <p className='sign-up'>Already have an account? Sign In <Link to='/login' className='sign-up-sign'>here</Link>!</p>
                         <input type="submit" value='Sign Up' className='submit-button'/>
                     </div>
-                    <div className='container-right'></div>
                 </form>
             </div>
         </div>
@@ -86,11 +85,11 @@ const Register = () => {
     //                 <input type="password" name="password" required/>
     //             </div>
     //             <div className="button-container">
-    //                 <input type="submit" value="Register"/>
+    //                 <input type="submit" value="Signup"/>
     //             </div>
     //         </form>
     //     </div>
     // )
 }
 
-export default Register;
+export default Signup;
